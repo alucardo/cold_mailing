a57a7c8dcddffc07fd6266f01d3c82fdf5022301
Przemysław Olesiński <kontakt@przemyslawolesinski.pl>
2026-02-08 09:55:50 +0100

    Add styles to dashboard and settings

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import ApiType, ApiSetting
+from .forms import CreateApiForm
 
 # Create your views here.
-def apis(request):
-    return render(request, 'settings/apis.html')
+def apis_view(request):
+    api_list = ApiSetting.objects.all()
+    context = {'api_list': api_list}
+    return render(request, 'settings/apis.html', context)
+
+def create_api_view(request):
+    if request.method == 'POST':
+        form = CreateApiForm(request.POST)
+        if form.is_valid():
+            api = ApiSetting(
+                name = form.cleaned_data['name'],
+                text = form.cleaned_data['text'],
+                api_type_id = 1
+            )
+            api.save()
+            return HttpResponseRedirect('/settings/apis')
+    else:
+        form = CreateApiForm()
+    return render(request, 'settings/create_api.html', {'form': form})
