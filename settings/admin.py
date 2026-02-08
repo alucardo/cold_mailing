ad2efbf5ef1889147e92894f85284647c00bfd41
Przemysław Olesiński <kontakt@przemyslawolesinski.pl>
2026-02-08 09:56:37 +0100

    Register api type and settings to admin site

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ApiSetting, ApiType
 
 # Register your models here.
+
+class ApiTypeAdmin(admin.ModelAdmin):
+    pass
+
+class ApiSettingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'api_type', 'created_at']
+
+admin.site.register(ApiType, ApiTypeAdmin)
+admin.site.register(ApiSetting, ApiSettingAdmin)
